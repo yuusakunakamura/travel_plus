0c846a3e943425059a9b3d0afc47517adf708423
yuusakunakamura <kickyuu5@gmail.com>
2020-08-22 08:51:03 +0000

    add refire key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a41e6d755a91be5a5ffc6d1a2835cd91e9cc22010421d8c6d0b3eab6417c56c5dabe218ea4623f1e0039c3c95eaf3dd6844efcec9c55372501b4fc257939ccbf'
