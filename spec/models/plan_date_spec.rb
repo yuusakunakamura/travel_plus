d9bd92feb76c0fd6190c281802bdb7901fcf9eec
yuusakunakamura <kickyuu5@gmail.com>
2020-09-15 12:19:29 +0000

    rspec plan

--- spec/models/plan_date_spec.rb
+++ spec/models/plan_date_spec.rb
@@ -3,10 +3,17 @@ describe PlanDate do
    describe '#create' do
      it '投稿できる' do
       user = FactoryBot.create(:user)
-      user.plan = FactoryBot.create(:plan)
-      plan_date = build(:plan_date, user_id: user.id, plan_id: user.plan.id)
+      plan = FactoryBot.create(:plan,user_id: user.id)
+      plan_date = build(:plan_date, plan_id: plan.id)
       plan_date.valid?
       expect(plan_date).to be_valid
     end
+     it 'place_nameがなくて投稿できない' do
+      user = FactoryBot.create(:user)
+      plan = FactoryBot.create(:plan,user_id: user.id)
+      plan_date = build(:plan_date, plan_id: plan.id,place_name: "")
+      plan_date.valid?
+      expect(plan_date.errors[:place_name]).to include("を入力してください")
+    end
    end
-end
+end 
