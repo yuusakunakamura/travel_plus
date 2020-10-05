add2e8721e6a2521e955fff8f5476300e6143a53
yuusakunakamura <kickyuu5@gmail.com>
2020-10-05 13:30:31 +0000

    spec

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe Plan do
    describe '#create' do
-     it '投稿できる' do
+     it '投稿ができる' do
       user = FactoryBot.create(:user)
       plan = build(:plan, user_id: user.id)
       plan.valid?
